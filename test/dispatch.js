eb954e489c1a0f3c92111060f27d4278a8d5d2c4
1000copy <1000copy@gmail.com>
2023-06-29 18:29:55 +0800

    dispatch

--- test/dispatch.js
+++ test/dispatch.js
@@ -1,16 +1,18 @@
-var jd = require('dispatch')
-var check = require("check")
-class Obj1{
-    action1(){
-        result = "action1 of obj1 is here"
-        return result;
+(async ()=>{
+    var jd = require('dispatch')
+    var check = require("check")
+    class Obj1{
+        action1(){
+            result = "action1 of obj1 is here"
+            return result;
+        }
+        action2(params){
+            return params;
+        }
     }
-    action2(params){
-        return params;
-    }
-}
-jd.register('obj1',new Obj1())
-var result = jd.dispatch({resource:"obj1",action:"action1",params:{}})
-check( "action1 of obj1 is here",result.data,"failure of obj1.action1 dispatch")
-var result = jd.dispatch({resource:"obj1",action:"action2",params:{id:1}})
-check( {id:1},result.data,"failure of obj1.action2 dispatch")
+    jd.register('obj1',new Obj1())
+    var result = await jd.dispatch({resource:"obj1",action:"action1",params:{}})
+    check( "action1 of obj1 is here",result.data,"failure of obj1.action1 dispatch")
+    var result = await jd.dispatch({resource:"obj1",action:"action2",params:{id:1}})
+    check( {id:1},result.data,"failure of obj1.action2 dispatch")
+})()
--- test/tododispatch.js
+++ test/tododispatch.js
@@ -1,27 +1,29 @@
-var jd = require('dispatch')
-var check = require("check")
-class Obj1{
-    action1(){
-        result = "action1 of obj1 is here"
-        return result;
-    }
-    action2(params){
-        return params;
-    }
-}
-jd.register('obj1',new Obj1())
-var result = jd.dispatch({resource:"obj1",action:"action1",params:{}})
-check( "action1 of obj1 is here",result.data,"failure of obj1.action1 dispatch")
-var result = jd.dispatch({resource:"obj1",action:"action2",params:{id:1}})
-check( {id:1},result.data,"failure of obj1.action2 dispatch")
 (async ()=>{
+    var jd = require('dispatch')
+    var check = require("check")
+    class Obj1{
+        action1(){
+            result = "action1 of obj1 is here"
+            return result;
+        }
+        action2(params){
+            return params;
+        }
+    }
+    jd.register('obj1',new Obj1())
+    var result = await jd.dispatch({resource:"obj1",action:"action1",params:{}})
+    check( "action1 of obj1 is here",result.data,"failure of obj1.action1 dispatch")
+    var result = await jd.dispatch({resource:"obj1",action:"action2",params:{id:1}})
+    check( {id:1},result.data,"failure of obj1.action2 dispatch")
+
     var Todo = require('../src/todo')
     var check = require("check")
     var JsonFile = require("jsonfile")
     var jsonfile = new JsonFile('./test/todo.json')
     var todo = new Todo(jsonfile)
-    check(true,true)
-    await todo.add({id:1,subject:"todo 1"})
+    jd.register('todo',todo)
+    await jd.dispatch({resource:"todo",action:"add",params:{id:1,subject:"todo 1"}})
+    // await todo.add({id:1,subject:"todo 1"})
     // return 
     check(await todo.reload(),[{id:1,subject:"todo 1"}],'reload failure',true)
     check(await todo.remove({id:1}),{id:1},'todo.remove failure',true)
