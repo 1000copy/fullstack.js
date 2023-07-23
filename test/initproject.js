fe2dddbf2265697968ca6116a62bc265ea7c0928
1000copy <1000copy@gmail.com>
2023-07-24 07:17:12 +0800

    showprojectSPA

--- test/initproject.js
+++ test/initproject.js
@@ -12,8 +12,8 @@
 	todo.clear()
 	todo.add({id:1,subject:"todo 1",pid:1,checked:true})
 	todo.add({id:2,subject:"todo 2",pid:1,checked:true})
-	todo.add({id:1,subject:"todo 11",pid:2,checked:true})
-	todo.add({id:2,subject:"todo 22",pid:2,checked:true})
+	todo.add({id:3,subject:"todo 11",pid:2,checked:true})
+	todo.add({id:4,subject:"todo 22",pid:2,checked:true})
 	check(todo.list({pid:1}),[ 
 		{ id: 1, subject: 'todo 1', checked: true, pid: 1 },
 		{ id: 2, subject: 'todo 2', checked: true, pid: 1 } 
